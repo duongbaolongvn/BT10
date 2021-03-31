003938a4aa9933bf5675956a17007593bc8fff0f
duongbaolongvn <77760726+duongbaolongvn@users.noreply.github.com>
2021-03-31 16:25:28 +0700

    Add files via upload
    commit

---
+++ BT10/a4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct Point
+{
+    double x, y;
+    Point() {};
+    Point (double _x, double _y) : x(_x) , y(_y){};
+    void print (){
+        cout << "Toa do x: " << x << endl;
+        cout << "Toa do y: " << y << endl;
+    }
+};
+
+int main()
+{
+    Point A(4, 5);
+    A.print();
+    cout << "Dia chi cua A: " << &A << endl;  //dia chi cua A trung voi x
+    cout << "Dia chi cua toa do x: " << &(A.x) << endl; //dia chi cua y cach x 1 int (4 byte)
+    cout << "Dia chi cua toa do y: " << &(A.y) << endl;
+    return 0;
+}
---
+++ BT10/c1.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct String
+{
+    int n;
+    char* str = new char[n];
+    String(const char* xau) {
+        n = strlen(xau);
+        for (int i = 0; i < n; i++) {
+            str[i] = xau[i];
+        }
+    };
+    ~String() {
+        delete [] str;
+    }
+    void print() const {
+        cout << str << endl;
+    }
+    void append(const char* xauNoi) {
+        const int len = strlen(xauNoi);
+        char* temp = new char[n + len];
+        for (int i = 0; i < n; i++) {
+            temp[i] = str[i];
+        }
+        for (int i = 0; i < len; i++) {
+            temp[i + n] = xauNoi[i];
+        }
+        temp[n + len] = '\0';
+        str = temp;
+        cout << str << endl;
+    }
+};
+
+int main()
+{
+    String s("xin chào");
+    s.print();
+    String greeting("chào");
+	greeting.append("....");
+    return 0;
+}
